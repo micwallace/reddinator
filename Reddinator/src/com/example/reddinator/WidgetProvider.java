fc1773e3aaf5f5bdc857618c7b6757007e8d1851
micwallace <micwallace@gmx.com>
2013-03-16 15:09:50 +1100

    messy

---
+++ Reddinator/src/com/example/reddinator/WidgetProvider.java
@@ -0,0 +1,14 @@
+package com.example.reddinator;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class WidgetProvider extends AppWidgetProvider {
+	public WidgetProvider() {
+	}
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		
+	}
+}
